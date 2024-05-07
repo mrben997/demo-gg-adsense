e37796e8e3ae261f0bf18125f4b1e107ed3aabfd
mrben997 <ngduth97@gmail.com>
2024-05-07 15:07:43 +0700

    update

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
+import { Adsense } from "./components/adsense";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -16,7 +17,23 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <Adsense
+          className="adsbygoogle"
+          style={{ display: "block" }}
+          client="ca-pub-9643912173816808"
+          slot="1480448417"
+          format="auto"
+          responsive="true"
+        ></Adsense>
+        {children}
+        <script
+          async
+          src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-9643912173816808"
+          crossOrigin="anonymous"
+        ></script>
+        {/* <script>(adsbygoogle = window.adsbygoogle || []).push({});</script> */}
+      </body>
     </html>
   );
 }
